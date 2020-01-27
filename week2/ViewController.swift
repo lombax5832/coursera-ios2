5d0160fbf8a79119eb34e8122ca7bc6ae64306da
Alexei Snytkine <iPodAlexei@gmail.com>
2020-01-27 07:33:07 -0500

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by Alexei Snytkine on 1/27/20.
+//  Copyright © 2020 Alexei Snytkine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
