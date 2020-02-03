a0e6e7415ee2cc1e6968b28c1342213c74153d61
Alexei Snytkine <iPodAlexei@gmail.com>
2020-02-03 02:08:12 -0500

    week4-1

--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -13,58 +13,52 @@ class ViewController: UIViewController {
     var filteredImage: UIImage?
     var originalImage: UIImage?
     
+    @IBOutlet weak var bottomMenu: UIStackView!
+    @IBOutlet var secondaryMenu: UIView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var filterButton: UIButton!
     
-    @IBAction func switchImage(_ sender: UISwitch) {
-        if(sender.isOn){
-            imageView.image = filteredImage
-        }else{
-            imageView.image = originalImage
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        originalImage = UIImage(named: "scenery.png")!
-        let rgbaImage = RGBAImage(image: originalImage!)!
+        secondaryMenu.backgroundColor = secondaryMenu.backgroundColor?.withAlphaComponent(0.5)
+        
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+        
+        originalImage = UIImage(named: "scenery.png")
         
         imageView.image = originalImage
-        
-        //let pixelCount = rgbaImage.width * rgbaImage.height
-        let avgRed = 107
-        //let avgGreen = 109
-        //let avgBlue = 110
-        //let sum = avgRed + avgGreen + avgBlue
-        
-        for y in 0..<rgbaImage.height {
-            for x in 0..<rgbaImage.width {
-                let index = y*rgbaImage.width + x
-                
-                var pixel = rgbaImage.pixels[index]
-                
-                let redDelta = Int(pixel.red) - avgRed
-                //let greenDelta = Int(pixel.green) - avgGreen
-                //let blueDelta = Int(pixel.blue) - avgBlue
-                
-                var modifier = 1 + 10*(Double(y)/Double(rgbaImage.height))
-                if (Int(pixel.red)<avgRed) {
-                    modifier = 1
-                }
-                
-                pixel.red = UInt8(max(min(255, Int(round(Double(avgRed) + modifier*Double(redDelta)))), 0))
-                
-                rgbaImage.pixels[index] = pixel
-            }
-        }
-        
-        filteredImage = rgbaImage.toUIImage()
-        
-        print("This code has executed")
-        
-        //imageView.image = result
     }
 
-
+    @IBAction func onFilter(_ sender: UIButton) {
+        if(sender.isSelected){
+            hideSecondaryMenu()
+            sender.isSelected = false
+        }else{
+            showSecondaryMenu()
+            sender.isSelected = true
+        }
+        
+    }
+    
+    func showSecondaryMenu(){
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraint(equalTo: bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        let heightConstraint = secondaryMenu.heightAnchor.constraint(equalToConstant: 44)
+        
+        NSLayoutConstraint.activate([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+    }
+    
+    func hideSecondaryMenu(){
+        secondaryMenu.removeFromSuperview()
+    }
+    
 }
 
